e5fd6972d5515950a5aba8252687fa9c548021a6
Jeevan Thiruchittampalam <jeevanthiruchittampalam@Jeevans-Air.localdomain>
2025-08-04 16:35:18 -0700

    Updated image paths to public folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,19 @@
-// --- page.js ---
-
 'use client';
 
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 import GroupWheel from './components/GroupWheel';
 
-// Backgrounds mapped to sector names
-import InvestmentsImg from './images/Plant2.jpg';
-import DevelopmentImg from './images/DevelopmentC.jpg';
-import TechnologyImg from './images/Technology.jpg';
-import ManagementImg from './images/Management.jpg';
-import PropertiesImg from './images/Properties2.jpg';
-import OtherIndustriesImg from './images/OtherIndustries.jpg';
-
+// Backgrounds mapped to sector names (use public image paths)
 const sectorBackgrounds = {
-  'Investments and Loans': InvestmentsImg,
-  'Development': DevelopmentImg,
-  'Technology': TechnologyImg,
-  'Management': ManagementImg,
-  'Properties': PropertiesImg,
-  'Other Industries': OtherIndustriesImg,
+  'Investments and Loans': '/images/Plant2.jpg',
+  'Development': '/images/DevelopmentC.jpg',
+  'Technology': '/images/Technology.jpg',
+  'Management': '/images/Management.jpg',
+  'Properties': '/images/Properties2.jpg',
+  'Other Industries': '/images/OtherIndustries.jpg',
 };
 
 const sectors = Object.keys(sectorBackgrounds);
@@ -51,13 +41,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-screen font-sans text-black">
-      {/* Header Component */}
       <Header />
 
-      {/* Main Content Section */}
       <main
         className="flex-grow flex flex-col items-center justify-center text-center px-4 py-16 bg-cover bg-center transition-all duration-500"
-        style={{ backgroundImage: `url(${sectorBackgrounds[activeSector].src})` }}
+        style={{
+          backgroundImage: `url(${sectorBackgrounds[activeSector]})`,
+        }}
       >
         <h1 className="text-4xl font-bold text-white mb-4 drop-shadow-lg">
           Welcome to Seville Management Ltd.
@@ -66,7 +56,6 @@ export default function Home() {
           We manage and develop exceptional living spaces with care and expertise.
         </p>
 
-        {/* Search & Group Wheel */}
         <SearchBar />
         <GroupWheel
           activeSector={activeSector}
@@ -74,7 +63,6 @@ export default function Home() {
         />
       </main>
 
-      {/* Footer Component */}
       <Footer />
     </div>
   );
